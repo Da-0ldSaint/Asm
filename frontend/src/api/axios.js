7af684b522189fc3d8936885ca14df104ff946c6
ec2 <ec2@ec2>
2026-02-28 18:09:37 +0000

    new conf

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://asm_backend:5000/api',
+    baseURL: '/api',
     headers: {
         'Content-Type': 'application/json',
     },
